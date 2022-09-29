9e1e26a8f82c22339c1692125793c74dbbad2d31
Claudia Quiceno <claudiaeqr@gmail.com>
2022-09-29 12:52:07 -0500

    Primero Ejercicios En C# para la rama C#

---
+++ Project_Euler_Claudia/Ejercicio2.cs
@@ -0,0 +1,21 @@
+using System.Consola;
+class Fibonnacci
+{
+    static void Main(string[] args)
+    {
+        int suma_fibo_par = 2, i = 1, j = 2, suma = 0; 
+        while(i < 4000000)
+        {
+            suma = i + j;
+            i = j
+            j = suma
+            Console.WriteLine(suma)
+            if(suma % 2 == 0)
+            {
+                Console.WriteLine("suma es par"+suma)
+                suma_fibo_par += suma
+            }
+        }
+        Console.WriteLine("fibo suma= ", suma_fibo_par);
+    }
+}
