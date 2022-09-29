8c0cbf0ef47355caf3f3cf4ec3751c4a45e81ffd
Claudia Quiceno <claudiaeqr@gmail.com>
2022-09-29 13:38:59 -0500

    Ejercicio fibo para la rama C#

--- fiboClaudia/fiboClaudia/Program.cs
+++ fiboClaudia/fiboClaudia/Program.cs
@@ -1,4 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hola Mundo");
 
+int suma_fibo_par = 2, i = 1, j = 2, suma = 0;
+while (i < 4000000)
+{
+    suma = i + j;
+    i = j;
+    j = suma;
+    Console.WriteLine(suma);
+    if (suma % 2 == 0)
+    {
+        Console.WriteLine("suma es par" + suma);
+        suma_fibo_par += suma;
+    }
+}
+Console.WriteLine("fibo suma= ", suma_fibo_par);
+
+
+Console.ReadKey();
